c8cb3babc1b59260c0b80144ca4c9a4b68e8e8cf
ataker54 <arsik54063@gmail.com>
2025-11-02 18:59:28 +0300

    wip(need fix reconnect)

--- gps_tracker.cpp
+++ gps_tracker.cpp
@@ -1,37 +1,90 @@
 #include "gps_tracker.h"
+#include <QDebug>
 
-#include "ui_gps_tracker.h"
-#include <QtDebug>
-#include <QSerialPortInfo>
-#include <QString>
-#include <gps_port_autodetector.h>
-
-gps_tracker::gps_tracker(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::gps_tracker)
+gps_tracker::gps_tracker(QWidget *parent) : QWidget(parent)
 {
-    ui->setupUi(this);
-    connect(ui->btn, SIGNAL(pressed()),this,SLOT(btn_click()));
+    btnStart = new QPushButton("Старт GPS", this);
+    btnStop = new QPushButton("Стоп GPS", this);
+    gpsView = new QTextEdit(this);
+    gpsView->setReadOnly(true);
+
+    layout = new QVBoxLayout(this);
+    layout->addWidget(btnStart);
+    layout->addWidget(btnStop);
+    layout->addWidget(gpsView);
+    setLayout(layout);
+
+    connect(btnStart, &QPushButton::clicked, this, &gps_tracker::startGps);
+    connect(btnStop, &QPushButton::clicked, this, &gps_tracker::stopGps);
 }
 
 gps_tracker::~gps_tracker()
 {
-    delete ui;
+    stopGps();
 }
 
-
-
-void gps_tracker::btn_click()
+void gps_tracker::startGps()
 {
-    GpsPortAutoDetector detector;
-    detector.FindPorts();
+    if (gpsThread) return;
 
-    QString gpsPort = detector.getGpsPortName();
-    if (gpsPort.isEmpty()) {
-        qDebug() << "GPS-порт не найден.";
-        return;
-    }
-    GPSReceiver *receiver = new GPSReceiver(this);
-    receiver->start(gpsPort, 9600);
+    gpsThread = new QThread(this);
+    receiver = new GPSReceiver();
+    receiver->isWriteToDebug = false;
+    receiver->isWriteToFile = false;
+
+    receiver->moveToThread(gpsThread);
+
+    connect(gpsThread, &QThread::started, [=]() {
+        receiver->start("COM6", 9600); // замените на нужный порт
+    });
+
+    connect(receiver, &GPSReceiver::gpsUpdated, this, &gps_tracker::updateGpsView);
+    connect(receiver, &GPSReceiver::finished, gpsThread, &QThread::quit);
+    connect(gpsThread, &QThread::finished, receiver, &QObject::deleteLater);
+    connect(gpsThread, &QThread::finished, gpsThread, &QObject::deleteLater);
+
+    gpsThread->start();
+    qDebug() << "✅ GPS поток запущен.";
 }
+
+void gps_tracker::stopGps()
+{
+    if (receiver) {
+        receiver->stop();
+        receiver = nullptr;
+    }
+
+    if (gpsThread) {
+        gpsThread->quit();
+        gpsThread->wait();
+        gpsThread = nullptr;
+        qDebug() << "🛑 GPS поток остановлен.";
+    }
+
+    gpsView->clear();
+}
+
+void gps_tracker::updateGpsView(const GpsData &data)
+{
+    QString text;
+    text += "UTC Time   : " + data.timeUtc + "\n";
+    text += "Date       : " + data.date + "\n";
+    text += "Latitude   : " + QString::number(data.latitude, 'f', 6) + "\n";
+    text += "Longitude  : " + QString::number(data.longitude, 'f', 6) + "\n";
+    text += "Altitude   : " + QString::number(data.altitude, 'f', 2) + " m\n";
+    text += "Speed      : " + QString::number(data.speedKmh, 'f', 2) + " km/h\n";
+    text += "Course     : " + QString::number(data.course, 'f', 2) + "°\n";
+    text += "Satellites : " + QString::number(data.satellites) + "\n";
+    text += "Valid Fix  : " + QString(data.valid ? "Yes" : "No") + "\n";
+
+    gpsView->setPlainText(text);
+}
+
+
+
+
+
+
+
+
 
--- gps_tracker.h
+++ gps_tracker.h
@@ -1,26 +1,38 @@
 #ifndef GPS_TRACKER_H
 #define GPS_TRACKER_H
 
-#include <QMainWindow>
-#include <gps_receiver.h>
+#include <QWidget>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QThread>
+#include "gps_receiver.h"
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class gps_tracker; }
-QT_END_NAMESPACE
-
-class gps_tracker : public QMainWindow // fix: переименовать название класса в стиле UpperCamelCase
+class gps_tracker : public QWidget
 {
     Q_OBJECT
 
 public:
-    gps_tracker(QWidget *parent = nullptr);
+    explicit gps_tracker(QWidget *parent = nullptr);
     ~gps_tracker();
 
 private slots:
-    void btn_click();
+    void startGps();
+    void stopGps();
+    void updateGpsView(const GpsData &data);
 
 private:
-    Ui::gps_tracker *ui;
+    QPushButton *btnStart;
+    QPushButton *btnStop;
+    QTextEdit *gpsView;
+    QVBoxLayout *layout;
 
+    QThread *gpsThread = nullptr;
+    GPSReceiver *receiver = nullptr;
 };
+
+
+
+
+
 #endif // GPS_TRACKER_H
